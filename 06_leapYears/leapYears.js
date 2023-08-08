e1570de63ae4bff9a3693febefa0d8cb16bf669c
David Wu <34573725+david204code@users.noreply.github.com>
2023-08-08 17:38:09 +0100

    Completed 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
+
+const view = leapYears(2004);
+console.log(view);
 
 // Do not edit below this line
 module.exports = leapYears;
