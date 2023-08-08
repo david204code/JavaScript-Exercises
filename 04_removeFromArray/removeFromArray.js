474ba63a2eb1f99ed8a91189a6d504e6ced921be
David Wu <34573725+david204code@users.noreply.github.com>
2023-08-08 16:55:10 +0100

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  const newArray = [];
+  
+  array.forEach((item) => {
+    if(!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  return newArray
 };
+
+const view = removeFromArray([1, 2, 3, 4], 2, 4)
+console.log(view);
 
 // Do not edit below this line
 module.exports = removeFromArray;
