558b39189cd580e22800abba6dc947a40928af48
David Wu <34573725+david204code@users.noreply.github.com>
2023-08-08 16:01:20 +0100

    Completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  return word.split("").reverse().join("");
 };
+
+const view = reverseString("sknahT");
+console.log(view);
 
 // Do not edit below this line
 module.exports = reverseString;
